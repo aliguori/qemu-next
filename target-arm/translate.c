39a4991466041e6c2beaaec6586d6d7c1891f6f8
Juha Riihimäki <juha.riihimaki@nokia.com>
2009-08-20 12:35:22 +0300

    replace hack in gen_bx_im with correct code
    Signed-off-by: Juha Riihimäki <juha.riihimaki@nokia.com>

--- target-arm/translate.c
+++ target-arm/translate.c
@@ -686,14 +686,13 @@ static inline void gen_bx_im(DisasContext *s, uint32_t addr)
     TCGv tmp;
 
     s->is_jmp = DISAS_UPDATE;
-    tmp = new_tmp();
     if (s->thumb != (addr & 1)) {
+        tmp = new_tmp();
         tcg_gen_movi_i32(tmp, addr & 1);
         tcg_gen_st_i32(tmp, cpu_env, offsetof(CPUState, thumb));
+        dead_tmp(tmp);
     }
-    tcg_gen_movi_i32(tmp, addr & ~1);
-    tcg_gen_st_i32(tmp, cpu_env, offsetof(CPUState, regs[15]));
-    dead_tmp(tmp);
+    tcg_gen_movi_i32(cpu_R[15], addr & ~1);
 }
 
 /* Set PC and Thumb state from var.  var is marked as dead.  */
