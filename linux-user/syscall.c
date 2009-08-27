40ae3224d39a34a1fc3145f8dcb6cf282ff4c3b1
Riku Voipio <riku.voipio@nokia.com>
2009-08-27 17:26:13 +0300

    Revert "linux-user: add eventfd support"
    This reverts commit 49adb9601890420526d42d53837f9b486e6f9c71.

--- linux-user/syscall.c
+++ linux-user/syscall.c
@@ -6864,18 +6864,7 @@ abi_long do_syscall(void *cpu_env, int num, abi_long arg1,
         ret = get_errno(sys_inotify_rm_watch(arg1, arg2));
         break;
 #endif
-#ifdef CONFIG_EVENTFD
-#if defined(TARGET_NR_eventfd)
-    case TARGET_NR_eventfd:
-        ret = get_errno(eventfd(arg1, 0));
-        break;
-#endif
-#if defined(TARGET_NR_eventfd2)
-    case TARGET_NR_eventfd2:
-        ret = get_errno(eventfd(arg1, arg2));
-        break;
-#endif
-#endif
+
 #ifdef TARGET_NR_mq_open
     case TARGET_NR_mq_open:
         {
