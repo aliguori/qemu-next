73b4682c8c33e1a4a59c2ad1331f9de946d8d96e
Juha Riihimäki <juha.riihimaki@nokia.com>
2009-10-02 08:27:45 +0300

    fix arm neon resource leak
    Signed-off-by: Juha Riihimäki <juha.riihimaki@nokia.com>

--- target-arm/translate.c
+++ target-arm/translate.c
@@ -4585,8 +4585,7 @@ static int disas_neon_data_insn(CPUState * env, DisasContext *s, uint32_t insn)
                     } else { /* size < 3 */
                         /* Operands in T0 and T1.  */
                         tmp = neon_load_reg(rm, pass);
-                        tmp2 = new_tmp();
-                        tcg_gen_movi_i32(tmp2, shift_imm);
+                        tmp2 = new_const(shift_imm);
                         switch (op) {
                         case 0:  /* VSHR */
                         case 1:  /* VSRA */
@@ -4722,6 +4721,7 @@ static int disas_neon_data_insn(CPUState * env, DisasContext *s, uint32_t insn)
                             gen_neon_narrow_satu(size - 1, tmp, cpu_V0);
                     }
                     if (pass == 0) {
+                        dead_tmp(tmp2);
                         tmp2 = tmp;
                     } else {
                         neon_store_reg(rd, 0, tmp2);
