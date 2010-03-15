5508cc5fa956fcf583def7a1bdba85e338742132
Anthony Liguori <aliguori@us.ibm.com>
2010-03-15 13:38:01 -0500

    Switch to view widget

--- gtk/core.c
+++ gtk/core.c
@@ -4,6 +4,7 @@
 #include "gtk.h"
 #include "sysemu.h"
 #include "gtk/qemudisplay.h"
+#include "gtk/view/autoDrawer.h"
 
 typedef struct QemuDisplayChangeListener
 {
@@ -109,13 +110,65 @@ static void update_mouse_icon(QemuDisplay *obj, gpointer data)
     gtk_image_set_from_stock(GTK_IMAGE(icon), stock_id, GTK_ICON_SIZE_MENU);
 }
 
+static GtkWidget *replace_vbox_with_autodrawer(GtkWidget *vbox)
+{
+    GtkWidget *parent, *drawer;
+    GtkBoxChild *box_child;
+    GtkBoxChild child[2];
+    GtkBox *box = GTK_BOX(vbox);
+    int i;
+    gboolean expand, fill;
+    guint padding;
+    GtkPackType pack_type;
+    guint position;
+
+    g_assert(g_list_length(box->children) == 2);
+
+    for (i = 0; i < 2; i++) {
+        box_child = g_list_nth_data(box->children, i);
+        memcpy(&child[i], box_child, sizeof(child[i]));
+    }
+
+    parent = gtk_widget_get_parent(vbox);
+    drawer = ViewAutoDrawer_New();
+
+    for (i = 0; i < 2; i++) {
+        g_object_ref(child[i].widget);
+        gtk_container_remove(GTK_CONTAINER(vbox), child[i].widget);
+
+        g_object_get(G_OBJECT(child[i].widget), "position", &position, NULL);
+        if (position == 0) {
+            ViewOvBox_SetOver(VIEW_OV_BOX(drawer), child[i].widget);
+        } else {
+            ViewOvBox_SetUnder(VIEW_OV_BOX(drawer), child[i].widget);
+        }
+        g_object_unref(child[i].widget);
+    }
+
+    g_object_get(G_OBJECT(vbox), "position", &position, NULL);
+    gtk_box_query_child_packing(GTK_BOX(parent), vbox,
+                                &expand, &fill, &padding, &pack_type);
+    gtk_container_remove(GTK_CONTAINER(parent), vbox);
+
+    if (pack_type == GTK_PACK_START) {
+        gtk_box_pack_start(GTK_BOX(parent), drawer, expand, fill, padding);
+    } else {
+        gtk_box_pack_end(GTK_BOX(parent), drawer, expand, fill, padding);
+    }
+
+    gtk_box_reorder_child(GTK_BOX(parent), drawer, position);
+
+    return drawer;
+}
+
 void gtk_display_init(DisplayState *ds)
 {
-    GtkWidget *window, *display;
+    GtkWidget *window, *display, *vbox2, *drawer;
     GtkBuilder *builder;
     int ret;
     char *gtk_path;
     QemuDisplayChangeListener *dcl;
+    GError *error = NULL;
 
     gtk_init(NULL, NULL);
 
@@ -131,9 +184,17 @@ void gtk_display_init(DisplayState *ds)
     qemu_display_get_type();
 
     builder = gtk_builder_new();
-    gtk_builder_add_from_file(builder, "qemu-gui.xml", NULL);
+    gtk_builder_add_from_file(builder, "qemu-gui.xml", &error);
+    if (error != NULL) {
+        printf("%s\n", error->message);
+        exit(1);
+    }
 
+    vbox2 = GTK_WIDGET(gtk_builder_get_object(builder, "vbox2"));
     display = GTK_WIDGET(gtk_builder_get_object(builder, "display"));
+
+    drawer = replace_vbox_with_autodrawer(vbox2);
+    ViewAutoDrawer_SetActive(VIEW_AUTODRAWER(drawer), FALSE);
 
     qemu_display_set_host_key(QEMU_DISPLAY(display),
                               3, (gint[]){GDK_Control_L, GDK_Alt_L, GDK_a});
@@ -165,6 +226,7 @@ void gtk_display_init(DisplayState *ds)
     update_mouse_icon(QEMU_DISPLAY(display), builder);
 
     window = GTK_WIDGET(gtk_builder_get_object(builder, "window1"));
+    gtk_widget_show_all(window);
 
     g_signal_connect(G_OBJECT(window), "delete-event",
                      G_CALLBACK(close_window), NULL);
