6305d334534c608393112808a558bfd2c169f1a2
Markus Armbruster <armbru@redhat.com>
2010-03-31 13:25:15 -0300

    monitor: convert do_device_add() to QObject
    RH-Author: Markus Armbruster <armbru@redhat.com>
    Message-id: <1270041921-28969-61-git-send-email-armbru@redhat.com>
    Patchwork-id: 8225
    O-Subject: [PATCH 60/66] monitor: convert do_device_add() to QObject
    Bugzilla: 579470
    RH-Acked-by: Kevin Wolf <kwolf@redhat.com>
    RH-Acked-by: Juan Quintela <quintela@redhat.com>
    RH-Acked-by: Luiz Capitulino <lcapitulino@redhat.com>
    
    (cherry picked from commit 8bc27249f0f62524887ea355a6604722edd276a9)
    ---
     hw/qdev.c       |   42 ++++++++++++++++++++++++++++++++++--------
     hw/qdev.h       |    2 +-
     qemu-monitor.hx |    3 ++-
     3 files changed, 37 insertions(+), 10 deletions(-)
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- qemu-monitor.hx
+++ qemu-monitor.hx
@@ -545,7 +545,8 @@ ETEXI
         .args_type  = "device:O",
         .params     = "driver[,prop=value][,...]",
         .help       = "add device, like -device on the command line",
-        .mhandler.cmd = do_device_add,
+        .user_print = monitor_user_noop,
+        .mhandler.cmd_new = do_device_add,
     },
 
 STEXI
