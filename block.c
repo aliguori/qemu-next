afe34a2ea5bb35985fe5134f5ee8f9a672c19510
Naphtali Sprei <nsprei@redhat.com>
2010-04-07 16:44:16 -0300

    read-only: No need anymoe for bdrv_set_read_only
    RH-Author: Naphtali Sprei <nsprei@redhat.com>
    Message-id: <1270658667-14294-6-git-send-email-nsprei@redhat.com>
    Patchwork-id: 8440
    O-Subject: [RHEL6 kvm PATCH v3 rebase 05/16] read-only: No need anymoe for
    	bdrv_set_read_only
    Bugzilla: 537164
    RH-Acked-by: Daniel P. Berrange <berrange@redhat.com>
    RH-Acked-by: Christoph Hellwig <chellwig@redhat.com>
    RH-Acked-by: Juan Quintela <quintela@redhat.com>
    RH-Acked-by: Kevin Wolf <kwolf@redhat.com>
    
    BZ: https://bugzilla.redhat.com/show_bug.cgi?id=537164
    upstream commit: 37226ad946597126f5bce36f75f818b12fe4b5b8
    
    Signed-off-by: Naphtali Sprei <nsprei@redhat.com>
    ---
     block.c |    7 -------
     block.h |    1 -
     2 files changed, 0 insertions(+), 8 deletions(-)
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- block.c
+++ block.c
@@ -1002,13 +1002,6 @@ int bdrv_is_read_only(BlockDriverState *bs)
     return bs->read_only;
 }
 
-int bdrv_set_read_only(BlockDriverState *bs, int read_only)
-{
-    int ret = bs->read_only;
-    bs->read_only = read_only;
-    return ret;
-}
-
 int bdrv_is_sg(BlockDriverState *bs)
 {
     return bs->sg;
