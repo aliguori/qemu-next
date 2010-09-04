fc7a286479c0e8ff06bfebf7727d6b12a914a5b5
Anthony Liguori <aliguori@us.ibm.com>
2010-09-04 08:06:27 -0500

    qed: add copyright
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>
    Signed-off-by: Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>

--- block/qed.c
+++ block/qed.c
@@ -1,3 +1,17 @@
+/*
+ * QEMU Enhanced Disk Format
+ *
+ * Copyright IBM, Corp. 2010
+ *
+ * Authors:
+ *  Stefan Hajnoczi   <stefanha@linux.vnet.ibm.com>
+ *  Anthony Liguori   <aliguori@us.ibm.com>
+ *
+ * This work is licensed under the terms of the GNU LGPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ *
+ */
+
 #include "trace.h"
 #include "block_int.h"
 
