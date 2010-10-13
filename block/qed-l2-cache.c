9b7b7bdd20aba06b2b58e5179e3bf03611146912
Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>
2010-10-13 13:51:35 +0100

    qed: Fix COPYING.LIB LGPL license file references
    Signed-off-by: Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>

--- block/qed-l2-cache.c
+++ block/qed-l2-cache.c
@@ -7,7 +7,7 @@
  *  Anthony Liguori   <aliguori@us.ibm.com>
  *
  * This work is licensed under the terms of the GNU LGPL, version 2 or later.
- * See the COPYING file in the top-level directory.
+ * See the COPYING.LIB file in the top-level directory.
  *
  */
 
--- block/qed-lock.c
+++ block/qed-lock.c
@@ -7,7 +7,7 @@
  *  Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>
  *
  * This work is licensed under the terms of the GNU LGPL, version 2 or later.
- * See the COPYING file in the top-level directory.
+ * See the COPYING.LIB file in the top-level directory.
  *
  */
 
