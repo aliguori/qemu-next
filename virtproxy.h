33daab58180b03d1cfd94670cba0ad01366e2b2a
Michael Roth <mdroth@linux.vnet.ibm.com>
2010-11-08 08:29:57 -0600

    virtproxy: modify virtproxy base to accept a chardev
    This allows us to pass a chardev to virtproxy instead of a listening FD.
    This also breaks out the virtproxy packet processing routines so they can
    be used directly when the device calls the chardev write handler,
    and wraps writes to the guest with a function that calls the device's
    write handler, or just writes to the channel fd, depending on the
    context (chardev (host, qemu) vs. channel fd (guest, qemu-vp)).

--- virtproxy.h
+++ virtproxy.h
@@ -16,8 +16,13 @@
 
 #include "qemu-common.h"
 #include "qemu-queue.h"
+#include "qemu-char.h"
 
 typedef struct VPDriver VPDriver;
+enum vp_context {
+    VP_CTX_CHARDEV, /* in qemu/host, channel is a virtproxy chardev */
+    VP_CTX_FD,      /* in guest, channel is an FD */
+};
 
 /* wrappers for s/vp/qemu/ functions we need */
 int vp_send_all(int fd, const void *buf, int len1);
@@ -30,11 +35,13 @@ int vp_set_fd_handler(int fd,
                         IOHandler *fd_read,
                         IOHandler *fd_write,
                         void *opaque);
+void vp_chr_read(CharDriverState *s, uint8_t *buf, int len);
 
 /* virtproxy interface */
-VPDriver *vp_new(int fd, bool listen);
+VPDriver *vp_new(enum vp_context ctx, CharDriverState *s, int fd, bool listen);
 int vp_set_oforward(VPDriver *drv, int fd, const char *service_id);
 int vp_set_iforward(VPDriver *drv, const char *service_id, const char *addr,
                     const char *port, bool ipv6);
+int vp_handle_packet_buf(VPDriver *drv, const void *buf, int count);
 
 #endif /* VIRTPROXY_H */
