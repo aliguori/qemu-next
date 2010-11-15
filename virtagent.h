4bbbf7e2a20200ae00ebe949cdd78688193d00ca
Michael Roth <mdroth@linux.vnet.ibm.com>
2010-11-14 23:28:47 -0600

    virtagent: add agent_ping header declaration

--- virtagent.h
+++ virtagent.h
@@ -30,6 +30,7 @@ int do_agent_viewdmesg(Monitor *mon, const QDict *mon_params,
                       MonitorCompletion cb, void *opaque);
 int do_agent_shutdown(Monitor *mon, const QDict *mon_params,
                       MonitorCompletion cb, void *opaque);
+void do_agent_ping_print(Monitor *mon, const QObject *qobject);
 int va_client_init(VPDriver *vp_drv, bool is_host);
 
 #endif /* VIRTAGENT_H */
