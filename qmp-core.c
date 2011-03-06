b9db17a830051a522cd98f2651c6ca1c7555b6a8
Anthony Liguori <aliguori@us.ibm.com>
2011-03-06 16:52:33 -0600

    qapi: add core QMP server support
    This is the infrastructure to register commands.
    
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

---
+++ qmp-core.c
@@ -0,0 +1,77 @@
+/*
+ * QAPI
+ *
+ * Copyright IBM, Corp. 2011
+ *
+ * Authors:
+ *  Anthony Liguori   <aliguori@us.ibm.com>
+ *
+ * This work is licensed under the terms of the GNU LGPL, version 2.  See
+ * the COPYING.LIB file in the top-level directory.
+ */
+#include "qmp.h"
+#include "qmp-core.h"
+#include "json-lexer.h"
+#include "json-parser.h"
+#include "json-streamer.h"
+#include "qemu_socket.h"
+#include <glib.h>
+#include "qemu-queue.h"
+#include "sysemu.h"
+
+typedef struct QmpCommand
+{
+    const char *name;
+    bool stateful;
+    QmpCommandFunc *fn;
+    QmpStatefulCommandFunc *sfn;
+    QTAILQ_ENTRY(QmpCommand) node;
+} QmpCommand;
+
+static QTAILQ_HEAD(, QmpCommand) qmp_commands =
+    QTAILQ_HEAD_INITIALIZER(qmp_commands);
+
+void qmp_register_command(const char *name, QmpCommandFunc *fn)
+{
+    QmpCommand *cmd = qemu_mallocz(sizeof(*cmd));
+
+    cmd->name = name;
+    cmd->stateful = false;
+    cmd->fn = fn;
+    QTAILQ_INSERT_TAIL(&qmp_commands, cmd, node);
+}
+
+void qmp_register_stateful_command(const char *name, QmpStatefulCommandFunc *fn)
+{
+    QmpCommand *cmd = qemu_mallocz(sizeof(*cmd));
+
+    cmd->name = name;
+    cmd->stateful = true;
+    cmd->sfn = fn;
+    QTAILQ_INSERT_TAIL(&qmp_commands, cmd, node);
+}
+
+char *qobject_as_string(QObject *obj)
+{
+    char buffer[1024];
+
+    switch (qobject_type(obj)) {
+    case QTYPE_QINT:
+        snprintf(buffer, sizeof(buffer), "%" PRId64,
+                 qint_get_int(qobject_to_qint(obj)));
+        return qemu_strdup(buffer);
+    case QTYPE_QSTRING:
+        return qemu_strdup(qstring_get_str(qobject_to_qstring(obj)));
+    case QTYPE_QFLOAT:
+        snprintf(buffer, sizeof(buffer), "%.17g",
+                 qfloat_get_double(qobject_to_qfloat(obj)));
+        return qemu_strdup(buffer);
+    case QTYPE_QBOOL:
+        if (qbool_get_int(qobject_to_qbool(obj))) {
+            return qemu_strdup("on");
+        }
+        return qemu_strdup("off");
+    default:
+        return NULL;
+    }
+}
