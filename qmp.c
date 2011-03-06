e3237339243f213fe4998f87f679c8c8f0af1ad6
Anthony Liguori <aliguori@us.ibm.com>
2011-03-06 17:08:00 -0600

    qapi: add query-version QMP command
    This is used internally by QMP.  It's also a pretty good example of a typical
    command conversion.
    
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

---
+++ qmp.c
@@ -0,0 +1,31 @@
+/*
+ * QAPI
+ *
+ * Copyright IBM, Corp. 2011
+ *
+ * Authors:
+ *  Anthony Liguori   <aliguori@us.ibm.com>
+ *
+ * This work is licensed under the terms of the GNU LGPL, version 2.  See
+ * the COPYING.LIB file in the top-level directory.
+ */
+#include "qemu-common.h"
+#include "qmp-core.h"
+#include "qmp.h"
+
+VersionInfo *qmp_query_version(Error **err)
+{
+    VersionInfo *info = qmp_alloc_version_info();
+    const char *version = QEMU_VERSION;
+    char *tmp;
+
+    info->qemu.major = strtol(version, &tmp, 10);
+    tmp++;
+    info->qemu.minor = strtol(tmp, &tmp, 10);
+    tmp++;
+    info->qemu.micro = strtol(tmp, &tmp, 10);
+    info->package = qemu_strdup(QEMU_PKGVERSION);
+
+    return info;
+}
+
