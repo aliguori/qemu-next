9c35a8c72a1960100de4880ff30277e5807783c7
Anthony Liguori <aliguori@us.ibm.com>
2011-03-11 15:30:31 -0600

    qapi: add copyright to qmp-types-core.h

--- qmp-types-core.h
+++ qmp-types-core.h
@@ -1,3 +1,14 @@
+/*
+ * QAPI
+ *
+ * Copyright IBM, Corp. 2011
+ *
+ * Authors:
+ *  Anthony Liguori   <aliguori@us.ibm.com>
+ *
+ * This work is licensed under the terms of the GNU LGPL, version 2.  See
+ * the COPYING.LIB file in the top-level directory.
+ */
 #ifndef QMP_TYPES_CORE_H
 #define QMP_TYPES_CORE_H
 
