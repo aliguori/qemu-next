fbe96cf9d7e595d26385888b96585540a353ace6
Anthony Liguori <aliguori@us.ibm.com>
2011-03-24 16:36:35 -0500

    convert qdict test

--- check-qdict.c
+++ check-qdict.c
@@ -9,7 +9,7 @@
  * This work is licensed under the terms of the GNU LGPL, version 2.1 or later.
  * See the COPYING.LIB file in the top-level directory.
  */
-#include <check.h>
+#include <glib.h>
 
 #include "qint.h"
 #include "qdict.h"
@@ -22,22 +22,21 @@
  * (with some violations to access 'private' data)
  */
 
-START_TEST(qdict_new_test)
+static void test_qdict_new(void)
 {
     QDict *qdict;
 
     qdict = qdict_new();
-    fail_unless(qdict != NULL);
-    fail_unless(qdict_size(qdict) == 0);
-    fail_unless(qdict->base.refcnt == 1);
-    fail_unless(qobject_type(QOBJECT(qdict)) == QTYPE_QDICT);
+    g_assert(qdict != NULL);
+    g_assert(qdict_size(qdict) == 0);
+    g_assert(qdict->base.refcnt == 1);
+    g_assert(qobject_type(QOBJECT(qdict)) == QTYPE_QDICT);
 
     // destroy doesn't exit yet
     free(qdict);
 }
-END_TEST
 
-START_TEST(qdict_put_obj_test)
+static void test_qdict_put_obj(void)
 {
     QInt *qi;
     QDict *qdict;
@@ -49,10 +48,10 @@ START_TEST(qdict_put_obj_test)
     // key "" will have tdb hash 12345
     qdict_put_obj(qdict, "", QOBJECT(qint_from_int(num)));
 
-    fail_unless(qdict_size(qdict) == 1);
+    g_assert(qdict_size(qdict) == 1);
     ent = QLIST_FIRST(&qdict->table[12345 % QDICT_BUCKET_MAX]);
     qi = qobject_to_qint(ent->value);
-    fail_unless(qint_get_int(qi) == num);
+    g_assert(qint_get_int(qi) == num);
 
     // destroy doesn't exit yet
     QDECREF(qi);
@@ -60,9 +59,8 @@ START_TEST(qdict_put_obj_test)
     qemu_free(ent);
     qemu_free(qdict);
 }
-END_TEST
 
-START_TEST(qdict_destroy_simple_test)
+static void test_qdict_destroy_simple(void)
 {
     QDict *qdict;
 
@@ -72,134 +70,160 @@ START_TEST(qdict_destroy_simple_test)
 
     QDECREF(qdict);
 }
-END_TEST
 
-static QDict *tests_dict = NULL;
-
-static void qdict_setup(void)
-{
-    tests_dict = qdict_new();
-    fail_unless(tests_dict != NULL);
-}
-
-static void qdict_teardown(void)
-{
-    QDECREF(tests_dict);
-    tests_dict = NULL;
-}
-
-START_TEST(qdict_get_test)
+static void test_qdict_get(void)
 {
     QInt *qi;
     QObject *obj;
     const int value = -42;
     const char *key = "test";
+    QDict *tests_dict = NULL;
+    tests_dict = qdict_new();
+    g_assert(tests_dict != NULL);
 
     qdict_put(tests_dict, key, qint_from_int(value));
 
     obj = qdict_get(tests_dict, key);
-    fail_unless(obj != NULL);
+    g_assert(obj != NULL);
 
     qi = qobject_to_qint(obj);
-    fail_unless(qint_get_int(qi) == value);
-}
-END_TEST
+    g_assert(qint_get_int(qi) == value);
 
-START_TEST(qdict_get_int_test)
+    QDECREF(tests_dict);
+}
+
+static void test_qdict_get_int(void)
 {
     int ret;
     const int value = 100;
     const char *key = "int";
+    QDict *tests_dict = NULL;
+    tests_dict = qdict_new();
+    g_assert(tests_dict != NULL);
 
     qdict_put(tests_dict, key, qint_from_int(value));
 
     ret = qdict_get_int(tests_dict, key);
-    fail_unless(ret == value);
-}
-END_TEST
+    g_assert(ret == value);
 
-START_TEST(qdict_get_try_int_test)
+    QDECREF(tests_dict);
+}
+
+static void test_qdict_get_try_int(void)
 {
     int ret;
     const int value = 100;
     const char *key = "int";
+    QDict *tests_dict = NULL;
+    tests_dict = qdict_new();
+    g_assert(tests_dict != NULL);
 
     qdict_put(tests_dict, key, qint_from_int(value));
 
     ret = qdict_get_try_int(tests_dict, key, 0);
-    fail_unless(ret == value);
-}
-END_TEST
+    g_assert(ret == value);
 
-START_TEST(qdict_get_str_test)
+    QDECREF(tests_dict);
+}
+
+static void test_qdict_get_str(void)
 {
     const char *p;
     const char *key = "key";
     const char *str = "string";
+    QDict *tests_dict = NULL;
+    tests_dict = qdict_new();
+    g_assert(tests_dict != NULL);
 
     qdict_put(tests_dict, key, qstring_from_str(str));
 
     p = qdict_get_str(tests_dict, key);
-    fail_unless(p != NULL);
-    fail_unless(strcmp(p, str) == 0);
-}
-END_TEST
+    g_assert(p != NULL);
+    g_assert(strcmp(p, str) == 0);
 
-START_TEST(qdict_get_try_str_test)
+    QDECREF(tests_dict);
+}
+
+static void test_qdict_get_try_str(void)
 {
     const char *p;
     const char *key = "key";
     const char *str = "string";
+    QDict *tests_dict = NULL;
+    tests_dict = qdict_new();
+    g_assert(tests_dict != NULL);
 
     qdict_put(tests_dict, key, qstring_from_str(str));
 
     p = qdict_get_try_str(tests_dict, key);
-    fail_unless(p != NULL);
-    fail_unless(strcmp(p, str) == 0);
-}
-END_TEST
+    g_assert(p != NULL);
+    g_assert(strcmp(p, str) == 0);
 
-START_TEST(qdict_haskey_not_test)
+    QDECREF(tests_dict);
+}
+
+static void test_qdict_haskey_not(void)
 {
-    fail_unless(qdict_haskey(tests_dict, "test") == 0);
-}
-END_TEST
+    QDict *tests_dict = NULL;
+    tests_dict = qdict_new();
+    g_assert(tests_dict != NULL);
 
-START_TEST(qdict_haskey_test)
+    g_assert(qdict_haskey(tests_dict, "test") == 0);
+
+    QDECREF(tests_dict);
+}
+
+static void test_qdict_haskey(void)
 {
     const char *key = "test";
+    QDict *tests_dict = NULL;
+    tests_dict = qdict_new();
+    g_assert(tests_dict != NULL);
 
     qdict_put(tests_dict, key, qint_from_int(0));
-    fail_unless(qdict_haskey(tests_dict, key) == 1);
-}
-END_TEST
+    g_assert(qdict_haskey(tests_dict, key) == 1);
 
-START_TEST(qdict_del_test)
+    QDECREF(tests_dict);
+}
+
+static void test_qdict_del(void)
 {
     const char *key = "key test";
+    QDict *tests_dict = NULL;
+    tests_dict = qdict_new();
+    g_assert(tests_dict != NULL);
 
     qdict_put(tests_dict, key, qstring_from_str("foo"));
-    fail_unless(qdict_size(tests_dict) == 1);
+    g_assert(qdict_size(tests_dict) == 1);
 
     qdict_del(tests_dict, key);
 
-    fail_unless(qdict_size(tests_dict) == 0);
-    fail_unless(qdict_haskey(tests_dict, key) == 0);
-}
-END_TEST
+    g_assert(qdict_size(tests_dict) == 0);
+    g_assert(qdict_haskey(tests_dict, key) == 0);
 
-START_TEST(qobject_to_qdict_test)
+    QDECREF(tests_dict);
+}
+
+static void test_qobject_to_qdict(void)
 {
-    fail_unless(qobject_to_qdict(QOBJECT(tests_dict)) == tests_dict);
-}
-END_TEST
+    QDict *tests_dict = NULL;
+    tests_dict = qdict_new();
+    g_assert(tests_dict != NULL);
 
-START_TEST(qdict_iterapi_test)
+    g_assert(qobject_to_qdict(QOBJECT(tests_dict)) == tests_dict);
+
+    QDECREF(tests_dict);
+}
+
+static void test_qdict_iterapi(void)
 {
     int count;
     const QDictEntry *ent;
+    QDict *tests_dict = NULL;
+    tests_dict = qdict_new();
+    g_assert(tests_dict != NULL);
 
-    fail_unless(qdict_first(tests_dict) == NULL);
+    g_assert(qdict_first(tests_dict) == NULL);
 
     qdict_put(tests_dict, "key1", qint_from_int(1));
     qdict_put(tests_dict, "key2", qint_from_int(2));
@@ -207,47 +231,57 @@ START_TEST(qdict_iterapi_test)
 
     count = 0;
     for (ent = qdict_first(tests_dict); ent; ent = qdict_next(tests_dict, ent)){
-        fail_unless(qdict_haskey(tests_dict, qdict_entry_key(ent)) == 1);
+        g_assert(qdict_haskey(tests_dict, qdict_entry_key(ent)) == 1);
         count++;
     }
 
-    fail_unless(count == qdict_size(tests_dict));
+    g_assert(count == qdict_size(tests_dict));
 
     /* Do it again to test restarting */
     count = 0;
     for (ent = qdict_first(tests_dict); ent; ent = qdict_next(tests_dict, ent)){
-        fail_unless(qdict_haskey(tests_dict, qdict_entry_key(ent)) == 1);
+        g_assert(qdict_haskey(tests_dict, qdict_entry_key(ent)) == 1);
         count++;
     }
 
-    fail_unless(count == qdict_size(tests_dict));
+    g_assert(count == qdict_size(tests_dict));
+
+    QDECREF(tests_dict);
 }
-END_TEST
 
 /*
  * Errors test-cases
  */
 
-START_TEST(qdict_put_exists_test)
+static void test_qdict_put_exists(void)
 {
     int value;
     const char *key = "exists";
+    QDict *tests_dict = NULL;
+    tests_dict = qdict_new();
+    g_assert(tests_dict != NULL);
 
     qdict_put(tests_dict, key, qint_from_int(1));
     qdict_put(tests_dict, key, qint_from_int(2));
 
     value = qdict_get_int(tests_dict, key);
-    fail_unless(value == 2);
+    g_assert(value == 2);
 
-    fail_unless(qdict_size(tests_dict) == 1);
+    g_assert(qdict_size(tests_dict) == 1);
+
+    QDECREF(tests_dict);
 }
-END_TEST
 
-START_TEST(qdict_get_not_exists_test)
+static void test_qdict_get_not_exists(void)
 {
-    fail_unless(qdict_get(tests_dict, "foo") == NULL);
+    QDict *tests_dict = NULL;
+    tests_dict = qdict_new();
+    g_assert(tests_dict != NULL);
+
+    g_assert(qdict_get(tests_dict, "foo") == NULL);
+
+    QDECREF(tests_dict);
 }
-END_TEST
 
 /*
  * Stress test-case
@@ -276,7 +310,7 @@ static QString *read_line(FILE *file, char *key)
 
 #define reset_file(file)    fseek(file, 0L, SEEK_SET)
 
-START_TEST(qdict_stress_test)
+static void test_qdict_stress(void)
 {
     size_t lines;
     char key[128];
@@ -286,11 +320,11 @@ START_TEST(qdict_stress_test)
     const char *test_file_path = "qdict-test-data.txt";
 
     test_file = fopen(test_file_path, "r");
-    fail_unless(test_file != NULL);
+    g_assert(test_file != NULL);
 
     // Create the dict
     qdict = qdict_new();
-    fail_unless(qdict != NULL);
+    g_assert(qdict != NULL);
 
     // Add everything from the test file
     for (lines = 0;; lines++) {
@@ -300,7 +334,7 @@ START_TEST(qdict_stress_test)
 
         qdict_put(qdict, key, value);
     }
-    fail_unless(qdict_size(qdict) == lines);
+    g_assert(qdict_size(qdict) == lines);
 
     // Check if everything is really in there
     reset_file(test_file);
@@ -314,9 +348,9 @@ START_TEST(qdict_stress_test)
         str1 = qstring_get_str(value);
 
         str2 = qdict_get_str(qdict, key);
-        fail_unless(str2 != NULL);
+        g_assert(str2 != NULL);
 
-        fail_unless(strcmp(str1, str2) == 0);
+        g_assert(strcmp(str1, str2) == 0);
 
         QDECREF(value);
     }
@@ -331,72 +365,40 @@ START_TEST(qdict_stress_test)
         qdict_del(qdict, key);
         QDECREF(value);
 
-        fail_unless(qdict_haskey(qdict, key) == 0);
+        g_assert(qdict_haskey(qdict, key) == 0);
     }
     fclose(test_file);
 
-    fail_unless(qdict_size(qdict) == 0);
+    g_assert(qdict_size(qdict) == 0);
     QDECREF(qdict);
 }
-END_TEST
 
-static Suite *qdict_suite(void)
+int main(int argc, char **argv)
 {
-    Suite *s;
-    TCase *qdict_public_tcase;
-    TCase *qdict_public2_tcase;
-    TCase *qdict_stress_tcase;
-    TCase *qdict_errors_tcase;
+    g_test_init(&argc, &argv, NULL);
 
-    s = suite_create("QDict test-suite");
+    g_test_add_func("/public/qdict_new", test_qdict_new);
+    g_test_add_func("/public/qdict_put_obj", test_qdict_put_obj);
+    g_test_add_func("/public/qdict_destroy_simple", test_qdict_destroy_simple);
 
-    qdict_public_tcase = tcase_create("Public Interface");
-    suite_add_tcase(s, qdict_public_tcase);
-    tcase_add_test(qdict_public_tcase, qdict_new_test);
-    tcase_add_test(qdict_public_tcase, qdict_put_obj_test);
-    tcase_add_test(qdict_public_tcase, qdict_destroy_simple_test);
+    g_test_add_func("/public/qdict_get", test_qdict_get);
+    g_test_add_func("/public/qdict_get_int", test_qdict_get_int);
+    g_test_add_func("/public/qdict_get_try_int", test_qdict_get_try_int);
+    g_test_add_func("/public/qdict_get_str", test_qdict_get_str);
+    g_test_add_func("/public/qdict_get_try_str", test_qdict_get_try_str);
+    g_test_add_func("/public/qdict_haskey_not", test_qdict_haskey_not);
+    g_test_add_func("/public/qdict_haskey", test_qdict_haskey);
+    g_test_add_func("/public/qdict_del", test_qdict_del);
+    g_test_add_func("/public/qobject_to_qdict", test_qobject_to_qdict);
+    g_test_add_func("/public/qdict_iterapi", test_qdict_iterapi);
 
-    /* Continue, but now with fixtures */
-    qdict_public2_tcase = tcase_create("Public Interface (2)");
-    suite_add_tcase(s, qdict_public2_tcase);
-    tcase_add_checked_fixture(qdict_public2_tcase, qdict_setup, qdict_teardown);
-    tcase_add_test(qdict_public2_tcase, qdict_get_test);
-    tcase_add_test(qdict_public2_tcase, qdict_get_int_test);
-    tcase_add_test(qdict_public2_tcase, qdict_get_try_int_test);
-    tcase_add_test(qdict_public2_tcase, qdict_get_str_test);
-    tcase_add_test(qdict_public2_tcase, qdict_get_try_str_test);
-    tcase_add_test(qdict_public2_tcase, qdict_haskey_not_test);
-    tcase_add_test(qdict_public2_tcase, qdict_haskey_test);
-    tcase_add_test(qdict_public2_tcase, qdict_del_test);
-    tcase_add_test(qdict_public2_tcase, qobject_to_qdict_test);
-    tcase_add_test(qdict_public2_tcase, qdict_iterapi_test);
-
-    qdict_errors_tcase = tcase_create("Errors");
-    suite_add_tcase(s, qdict_errors_tcase);
-    tcase_add_checked_fixture(qdict_errors_tcase, qdict_setup, qdict_teardown);
-    tcase_add_test(qdict_errors_tcase, qdict_put_exists_test);
-    tcase_add_test(qdict_errors_tcase, qdict_get_not_exists_test);
+    g_test_add_func("/errors/qdict_put_exists", test_qdict_put_exists);
+    g_test_add_func("/errors/qdict_get_not_exists", test_qdict_get_not_exists);
 
     /* The Big one */
-    qdict_stress_tcase = tcase_create("Stress Test");
-    suite_add_tcase(s, qdict_stress_tcase);
-    tcase_add_test(qdict_stress_tcase, qdict_stress_test);
+    g_test_add_func("/stress/qdict_stress", test_qdict_stress);
 
-    return s;
-}
+    g_test_run();
 
-int main(void)
-{
-	int nf;
-	Suite *s;
-	SRunner *sr;
-
-	s = qdict_suite();
-	sr = srunner_create(s);
-
-	srunner_run_all(sr, CK_NORMAL);
-	nf = srunner_ntests_failed(sr);
-	srunner_free(sr);
-
-	return (nf == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+    return 0;
 }
