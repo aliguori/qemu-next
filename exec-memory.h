ec8ac2a3ecfa0bad1b438181b4c91478bbd0886a
Avi Kivity <avi@redhat.com>
2011-06-26 13:50:30 +0300

    Internal interfaces for memory API
    get_system_memory() provides the root of the memory hierarchy.
    
    This interface is intended to be private between memory.c and exec.c.
    If this file is included elsewhere, it should be regarded as a bug (or
    TODO item).  However, it will be temporarily needed for the conversion
    to hierarchical memory routing.
    
    Signed-off-by: Avi Kivity <avi@redhat.com>

---
+++ exec-memory.h
@@ -0,0 +1,23 @@
+#ifndef EXEC_MEMORY_H
+#define EXEC_MEMORY_H
+
+/*
+ * Internal interfaces between memory.c/exec.c/vl.c.  Do not #include unless
+ * you're one of them.
+ */
+
+#include "memory.h"
+
+#ifndef CONFIG_USER_ONLY
+
+/* Get the root memory region.  This interface should only be used temporarily
+ * until a proper bus interface is available.
+ */
+MemoryRegion *get_system_memory(void);
+
+/* Set the root memory region.  This region is the system memory map. */
+void set_system_memory_map(MemoryRegion *mr);
+
+#endif
+
+#endif
