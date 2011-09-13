b477b20c81caa36411eb8125c39493498d93495a
Anthony Liguori <aliguori@us.ibm.com>
2011-09-13 10:47:55 -0500

    apic: rename apic.id -> apic.index
    This clashes with the qdev name property.  This shouldn't affect compatibility
    since the apic is a no_user device.
    
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- hw/pc.c
+++ hw/pc.c
@@ -876,7 +876,7 @@ static DeviceState *apic_init(void *env, uint8_t apic_id)
     static int apic_mapped;
 
     dev = qdev_create(NULL, "apic");
-    qdev_prop_set_uint8(dev, "id", apic_id);
+    qdev_prop_set_uint8(dev, "index", apic_id);
     qdev_prop_set_ptr(dev, "cpu_env", env);
     qdev_init_nofail(dev);
     d = sysbus_from_qdev(dev);
