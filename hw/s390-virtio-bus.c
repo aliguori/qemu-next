6221c915f235fdc551763e669d226da1597bcf37
Anthony Liguori <aliguori@us.ibm.com>
2011-12-09 12:56:16 -0600

    killall qdev_register

--- hw/s390-virtio-bus.c
+++ hw/s390-virtio-bus.c
@@ -384,7 +384,7 @@ static void s390_virtio_bus_register_withprop(VirtIOS390DeviceInfo *info)
     info->qdev.bus_info = &s390_virtio_bus_info;
 
     assert(info->qdev.size >= sizeof(VirtIOS390Device));
-    qdev_register(&info->qdev);
+    qdev_register_subclass(&info->qdev, TYPE_DEVICE);
 }
 
 static void s390_virtio_register(void)
--- hw/ssi.c
+++ hw/ssi.c
@@ -38,7 +38,7 @@ void ssi_register_slave(DeviceInfo *info)
     assert(info->size >= sizeof(SSISlave));
     info->init = ssi_slave_init;
     info->bus_info = &ssi_bus_info;
-    qdev_register(info);
+    qdev_register_subclass(info, TYPE_DEVICE);
 }
 
 DeviceState *ssi_create_slave(SSIBus *bus, const char *name)
