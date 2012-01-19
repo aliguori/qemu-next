a503730d027abb9ef9fabac3a0af59bba1ffc5ea
M. Mohan Kumar <mohan@in.ibm.com>
2012-01-19 12:21:12 +0530

    hw/9pfs: Preserve S_ISGID
    In passthrough security model in local fs driver, after a file creation
    chown and chmod are done to set the file credentials and mode as requested
    by 9p client. But if there was a request to create a file with S_ISGID
    bit, doing chown on that file resets the S_ISGID bit. So first call
    chown and then invoking chmod with proper mode bit retains the S_ISGID
    (if present/requested)
    
    This resulted in LTP mknod02, mknod03, mknod05, open10 test case
    failures. This patch fixes this issue.
    
    man 2 chown
    When the owner or group of an executable file are changed by an unprivileged
    user the S_ISUID  and  S_ISGID mode  bits are cleared.  POSIX does not specify
    whether this also should happen when root does the chown(); the Linux behavior
    depends on the kernel version.
    
    Signed-off-by: M. Mohan Kumar <mohan@in.ibm.com>
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>

--- hw/9pfs/virtio-9p-local.c
+++ hw/9pfs/virtio-9p-local.c
@@ -248,9 +248,6 @@ static int local_post_create_passthrough(FsContext *fs_ctx, const char *path,
 {
     char buffer[PATH_MAX];
 
-    if (chmod(rpath(fs_ctx, path, buffer), credp->fc_mode & 07777) < 0) {
-        return -1;
-    }
     if (lchown(rpath(fs_ctx, path, buffer), credp->fc_uid,
                 credp->fc_gid) < 0) {
         /*
@@ -260,6 +257,10 @@ static int local_post_create_passthrough(FsContext *fs_ctx, const char *path,
         if ((fs_ctx->export_flags & V9FS_SEC_MASK) != V9FS_SM_NONE) {
             return -1;
         }
+    }
+
+    if (chmod(rpath(fs_ctx, path, buffer), credp->fc_mode & 07777) < 0) {
+        return -1;
     }
     return 0;
 }
