f315f98ca632bcff8a121b252ce38fef421ce8ec
Paolo Bonzini <pbonzini@redhat.com>
2012-04-11 23:34:37 +0200

    cmdline: reindent numa_add
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- vl.c
+++ vl.c
@@ -943,49 +943,49 @@ static void numa_add(const char *optarg)
     int nodenr;
 
     optarg = get_opt_name(option, 128, optarg, ',') + 1;
-    if (!strcmp(option, "node")) {
-        if (get_param_value(option, 128, "nodeid", optarg) == 0) {
-            nodenr = nb_numa_nodes;
-        } else {
-            nodenr = strtoull(option, NULL, 10);
-        }
-
-        if (get_param_value(option, 128, "mem", optarg) == 0) {
-            node_mem[nodenr] = 0;
-        } else {
-            int64_t sval;
-            sval = strtosz(option, &endptr);
-            if (sval < 0 || *endptr) {
-                fprintf(stderr, "qemu: invalid numa mem size: %s\n", optarg);
-                exit(1);
-            }
-            node_mem[nodenr] = sval;
-        }
-        if (get_param_value(option, 128, "cpus", optarg) == 0) {
-            node_cpumask[nodenr] = 0;
-        } else {
-            value = strtoull(option, &endptr, 10);
-            if (value >= 64) {
-                value = 63;
-                fprintf(stderr, "only 64 CPUs in NUMA mode supported.\n");
-            } else {
-                if (*endptr == '-') {
-                    endvalue = strtoull(endptr+1, &endptr, 10);
-                    if (endvalue >= 63) {
-                        endvalue = 62;
-                        fprintf(stderr,
-                            "only 63 CPUs in NUMA mode supported.\n");
-                    }
-                    value = (2ULL << endvalue) - (1ULL << value);
-                } else {
-                    value = 1ULL << value;
-                }
-            }
-            node_cpumask[nodenr] = value;
-        }
-        nb_numa_nodes++;
+    if (strcmp(option, "node")) {
+        return;
     }
-    return;
+    if (get_param_value(option, 128, "nodeid", optarg) == 0) {
+        nodenr = nb_numa_nodes;
+    } else {
+        nodenr = strtoull(option, NULL, 10);
+    }
+
+    if (get_param_value(option, 128, "mem", optarg) == 0) {
+        node_mem[nodenr] = 0;
+    } else {
+        int64_t sval;
+        sval = strtosz(option, &endptr);
+        if (sval < 0 || *endptr) {
+            fprintf(stderr, "qemu: invalid numa mem size: %s\n", optarg);
+            exit(1);
+        }
+        node_mem[nodenr] = sval;
+    }
+    if (get_param_value(option, 128, "cpus", optarg) == 0) {
+        node_cpumask[nodenr] = 0;
+    } else {
+        value = strtoull(option, &endptr, 10);
+        if (value >= 64) {
+            value = 63;
+            fprintf(stderr, "only 64 CPUs in NUMA mode supported.\n");
+        } else {
+            if (*endptr == '-') {
+                endvalue = strtoull(endptr+1, &endptr, 10);
+                if (endvalue >= 63) {
+                    endvalue = 62;
+                    fprintf(stderr,
+                        "only 63 CPUs in NUMA mode supported.\n");
+                }
+                value = (2ULL << endvalue) - (1ULL << value);
+            } else {
+                value = 1ULL << value;
+            }
+        }
+        node_cpumask[nodenr] = value;
+    }
+    nb_numa_nodes++;
 }
 
 static int smp_init_func(QemuOpts *opts, void *opaque)
