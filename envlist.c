444aa1ed41f180a70add12ea6e4b705516185296
jim@meyering.net <jim@meyering.net>
2012-05-15 15:04:36 +0200

    envlist.c: handle strdup failure
    Without this, envlist_to_environ may silently fail to copy all
    strings into the destination buffer, and both callers would leak
    any env strings allocated after a failing strdup, because the
    freeing code stops at the first NULL pointer.
    
    Signed-off-by: Jim Meyering <meyering@redhat.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- envlist.c
+++ envlist.c
@@ -235,7 +235,14 @@ envlist_to_environ(const envlist_t *envlist, size_t *count)
 
 	for (entry = envlist->el_entries.lh_first; entry != NULL;
 	    entry = entry->ev_link.le_next) {
-		*(penv++) = strdup(entry->ev_var);
+		if ((*(penv++) = strdup(entry->ev_var)) == NULL) {
+			char **e = env;
+			while (e != penv) {
+				free(*e++);
+			}
+			free(env);
+			return NULL;
+		}
 	}
 	*penv = NULL; /* NULL terminate the list */
 
